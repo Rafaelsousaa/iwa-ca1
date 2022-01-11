08d9cfe75399729332ef99fe3bebe2b8544c5061
Rafaelsousaa <rafael.current@gmail.com>
2022-01-11 17:59:02 +0000

    someproblem debug

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
-
 const express = require('express')
 const app = express()
 const axios = require('axios');
 const cors = require('cors');
-const fs= require('fs')
+const fs = require('fs')
 app.use(express.json());
 app.use(express.urlencoded());
 
@@ -23,18 +22,18 @@ app.listen(5000, () => {
 //get all blog list
 app.get('/', (req, res) => {
     axios.get('http://localhost:5000/blog/list')
-        .then(function(response){
+        .then(function(response) {
             // return index and passs blogs as argument (list)
-            res.render('index', { blogs : response.data });
+            res.render('index', { blogs: response.data });
         })
-        .catch(err =>{
+        .catch(err => {
             res.send(err);
         })
 
 })
 
 app.get('/blog/list', (req, res) => {
-    
+
     const blog = getData()
     res.send(blog)
 })
@@ -42,35 +41,37 @@ app.get('/blog/list', (req, res) => {
 //update blog
 app.get('/update-blog/', (req, res) => {
     axios.get('http://localhost:5000/update-blog/:blogid')
-        .then(function(response){
+        .then(function(response) {
             //console.log(finds)
-           res.render('update_blog', {id: req.query.id,
-            blog: req.query.blog,
-            description: req.query.description,
-            title: req.query.title})
-            
+            res.render('update_blog', {
+                id: req.query.id,
+                blog: req.query.blog,
+                description: req.query.description,
+                title: req.query.title
+            })
+
         })
-        .catch(err =>{
+        .catch(err => {
             res.send(err);
         })
 
 })
 
 
-app.get('/update-blog/:blogid', (req, res)=> {
-       //params
-       const id = req.params.id
+app.get('/update-blog/:blogid', (req, res) => {
+    //params
+    const id = req.params.id
 
-       const blogId = getData()
+    const blogId = getData()
 
-       //check if it exists
-       const finds = blogId.filter( blog => blog.id === id)
-      if(finds){
-       res.send(finds)
-      }else{ 
-          res.status(404).send({ message : "Not found => id "+ id})
-          
-      }
+    //check if it exists
+    const finds = blogId.filter(blog => blog.id === id)
+    if (finds) {
+        res.send(finds)
+    } else {
+        res.status(404).send({ message: "Not found => id " + id })
+
+    }
 
 })
 
@@ -78,45 +79,45 @@ app.get('/update-blog/:blogid', (req, res)=> {
 //delete
 app.delete('/blog/delete/:id', (req, res) => {
     const id = req.params.id
-    //get the existing data
+        //get the existing data
     const blogId = getData()
-    //filter the data to remove it
-    const filterUser = blogId.filter( user => user.id !== id )
-    if ( blogId.length === filterUser.length ) {
-        return res.status(409).send({error: true, msg: 'id does not exist'})
+        //filter the data to remove it
+    const filterUser = blogId.filter(user => user.id !== id)
+    if (blogId.length === filterUser.length) {
+        return res.status(409).send({ error: true, msg: 'id does not exist' })
     }
     //save the data
     saveData(filterUser)
-    res.send({success: true, msg: 'Post removed successfully'})
-    
+    res.send({ success: true, msg: 'Post removed successfully' })
+
 })
 
 //post add
 app.post('/blog/add', (req, res) => {
 
-//get data
-const blogId = getData()
+    //get data
+    const blogId = getData()
 
-const blogData = req.body
+    const blogData = req.body
 
-//null or empty
-if (blogData.title == null || blogData.id == null || blogData.description == null || blogData.blog == null) {
-   return  res.status(500).send({
-        message : {error: true, msg: 'Data missing'}
-    });
-}
+    //null or empty
+    if (blogData.title == null || blogData.id == null || blogData.description == null || blogData.blog == null) {
+        return res.status(500).send({
+            message: { error: true, msg: 'Data missing' }
+        });
+    }
 
-//check if the blog exist already
-const findExist = blogId.find( blog => blog.id === blogData.id )
-if (findExist) {
-    return res.status(409).send({error: true, msg: 'Id already exist'})
-}
-//append data
-blogId.push(blogData)
-//save data
-saveData(blogId);
+    //check if the blog exist already
+    const findExist = blogId.find(blog => blog.id === blogData.id)
+    if (findExist) {
+        return res.status(409).send({ error: true, msg: 'Id already exist' })
+    }
+    //append data
+    blogId.push(blogData)
+        //save data
+    saveData(blogId);
 
- res.redirect('/');
+    res.redirect('/');
 })
 
 
@@ -125,31 +126,31 @@ saveData(blogId);
 app.patch('/blog/update/:id', (req, res) => {
     //get the id from url
     const id = req.params.id
-    //get the update data
+        //get the update data
     const blogData = req.body
-    //get the existing id
+        //get the existing id
     const blogId = getData()
-    //check if the id exist or not       
-    const findExist = blogId.find( blog => blog.id === id )
+        //check if the id exist or not       
+    const findExist = blogId.find(blog => blog.id === id)
     if (!findExist) {
-        return res.status(409).send({error: true, msg: 'id not exist'})
+        return res.status(409).send({ error: true, msg: 'id not exist' })
     }
     res.send(findExist);
-      //filter the data
-    const uBlog = blogId.filter( blog => blog.id !== id )
-    //push the updated data
+    //filter the data
+    const uBlog = blogId.filter(blog => blog.id !== id)
+        //push the updated data
     uBlog.push(blogData)
-    
+
     //finally save it
     saveData(uBlog)
-    
+
     res.redirect('/')
 })
 
 
 //save data on a json
 const saveData = (data) => {
-    
+
     const stringifyData = JSON.stringify(data)
     fs.writeFileSync('json/file.json', stringifyData)
 }
@@ -157,5 +158,5 @@ const saveData = (data) => {
 //get data from json
 const getData = () => {
     const jsonData = fs.readFileSync('json/file.json')
-    return JSON.parse(jsonData)    
+    return JSON.parse(jsonData)
 }
